b79bb67562184335e1a52d1c1fdee4e9d83fb3cf
10570628 <106284392+10570628@users.noreply.github.com>
2022-05-31 21:15:46 +0100

    Update Database_9.0.py
    To be final update. The database is set to find books when searched for the author. Connects t Library file and is currently functional.

--- Database 9.0/Database 9.0/Database_9.0.py
+++ Database 9.0/Database 9.0/Database_9.0.py
@@ -1,51 +1,66 @@
 import sqlite3
+from Library import Library
 
-conn = sqlite3.connect(':memory:')
+conn = sqlite3.connect(':memory:')    ###Refresh database on every test, do not need to delete database to add/remove info - does not retain info if books are removed###
 
 c = conn.cursor()
-
+###CREATE TABLE####
 c.execute("""CREATE TABLE library (
             first text,
             middle text,
             last text
             )""")
 
-
+                             ###INSERT BOOK###
 def insert_bok(bok):
     with conn:
         c.execute("INSERT INTO library VALUES (:first, :middle, :last)", {'first': bok.first, 'middle': bok.middle, 'last': bok.last})
 
-
-def get_bok_by_name(lastname):
+                             ###FIND BOOK### #(first name = title, Middle name = author, last = date book was published)#
+def get_bok_by_name(middlename):
     c.execute("SELECT * FROM library WHERE middle=:middle", {'middle': middlename})
+
     return c.fetchall()
 
-
-def update_last(bok, last):
+def update_last(bok, last):  ###Update book info###
     with conn:
         c.execute("""UPDATE library SET last = :last
                     WHERE first = :first AND middle = :middle""",
                   {'first': bok.first, 'middle': bok.middle, 'last': last})
 
 
-def remove_bok(bok):
+def remove_emp(bok):        ###remove book###
     with conn:
         c.execute("DELETE from library WHERE first = :first AND middle = :middle",
                   {'first': bok.first, 'middle': bok.middle})
 
-bok_1 = Book('Dracula', 'Bram Stoker', '26/05/1897')
-bok_2 = Book('Dune', 'Frank Herbert', '01/08/1865')
+                           ###Books to add to database - 8 in total###
+
+bok_1 = Library('Dracula','Bram Stoker', '26/05/1897')
+bok_2 = Library('Dune', 'Frank Herbert', '01/08/1865')
+bok_3 = Library('The Hobbit', 'J. R. R. Tolkien', '21/09/1937')
+bok_4 = Library('The Lord of the Rings, the Fellowship of the Ring', 'J. R. R. Tolkien', '29/07/1954')
+bok_5 = Library('The Lord of the Rings, the Two Towers', 'J. R. R. Tolkien', '11/11/1954')
+bok_6 = Library('The Lord of the Rings, the Return of the King', 'J. R. R. Tolkien', '20/10/1955')
+bok_7 = Library('Storm Front', 'Jim Butcher', '01/04/2000')
+bok_8 = Library('Fool Moon', 'Jim Butcher', '09/01/2001')
 
 insert_bok(bok_1)
 insert_bok(bok_2)
+insert_bok(bok_3)
+insert_bok(bok_4)
+insert_bok(bok_5)
+insert_bok(bok_6)
+insert_bok(bok_7)
+insert_bok(bok_8)
 
-boks = get_boks_by_name('Frank Herbert')
-print(emps)
 
+#to remove book - coded out to not remove something by default#
+#remove_emp(bok_1)
 
-remove_bok(bok_1)
+                    ###to find a book - it's set to by author (Middle) 
 
-boks = get_boks_by_name('Bram Stoker')
+boks = get_bok_by_name('Jim Butcher')
 print(boks)
-#conn.commit()
+conn.commit()
 conn.close()
